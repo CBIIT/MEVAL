33f03e9131d0b95088c47a74ae45dc5a844eb03d
qiongL910 <liuqiong891023@gmail.com>
2025-12-04 12:47:06 -0500

    try a fix to path bug

--- workflows/prefect/upsert_workflow.py
+++ workflows/prefect/upsert_workflow.py
@@ -11,7 +11,7 @@ from urllib.parse import urlparse
 import pandas as pd
 
 import sys
-sys.path.insert(0, os.path.abspath("./libs/prefect-toolkit/src"))
+sys.path.insert(0, os.path.abspath("../../libs/prefect-toolkit/src"))
 from commons.datamodel import GetDataModel
 
 
