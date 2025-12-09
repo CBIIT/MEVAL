7c7a0008dc602a3e2498ae2895889cbc34c1a526
qiongL910 <liuqiong891023@gmail.com>
2025-12-09 16:10:59 -0500

    get rid of task

--- workflows/prefect/upsert_workflow.py
+++ workflows/prefect/upsert_workflow.py
@@ -141,25 +141,24 @@ def combine_summaries(upsert_node_summary:dict, upser_rel_summary:dict) -> dict:
     return return_dict
 
 
-@task(name="Upsert nodes of a file", log_prints=True, cache_policy=NO_CACHE)
-def upsert_records_one_file(loader: Loader, file_path: str, id_field: str, subgraph_col: str, chunk_size: int = 3000):
-    """Prefect task to upsert data nodes from a submission file
-
-    Args:
-        loader (Loader): Loader instance
-        file_path (str): submission file path
-        id_field (str): id field to use for matching purpose
-        subgraph_col (str | None): The column indicating subgraph information. Defaults to None.
-        chunk_size (int, optional): Chunk size of each processing. Defaults to 3000.
-    """
-    file_upsert_summary = loader.upsert_file_records(file_path=file_path, id_field=id_field, subgraph_col=subgraph_col, chunk_size=chunk_size)
-    return file_upsert_summary
+#@task(name="Upsert nodes of a file", log_prints=True, cache_policy=NO_CACHE)
+#def upsert_records_one_file(loader: Loader, file_path: str, id_field: str, subgraph_col: str, chunk_size: int = 3000):
+#    """Prefect task to upsert data nodes from a submission file
+#
+#    Args:
+#        loader (Loader): Loader instance
+#        file_path (str): submission file path
+#        id_field (str): id field to use for matching purpose
+#        subgraph_col (str | None): The column indicating subgraph information. Defaults to None.
+#        chunk_size (int, optional): Chunk size of each processing. Defaults to 3000.
+#    """
+#    file_upsert_summary = loader.upsert_file_records(file_path=file_path, id_field=id_field, subgraph_col=subgraph_col, chunk_size=chunk_size)
+#    return file_upsert_summary
 
 
 @flow(
     name="Upsert nodes of file list",
     log_prints=True,
-    task_runner=ThreadPoolTaskRunner(max_workers=10),
 )
 def upsert_records_file_list(loader: Loader, file_list: list[str], id_field: str, subgraph_col: str|None = None, chunk_size: int = 3000):
     """Prefect flow to upsert data nodes from a list of submission files
@@ -171,52 +170,47 @@ def upsert_records_file_list(loader: Loader, file_list: list[str], id_field: str
         subgraph_col (str | None, optional): The column indicating subgraph information. Defaults to None.
         chunk_size (int, optional): Chunk size of each processing. Defaults to 3000.
     """
-
-    # Submit all tasks first
-    futures = upsert_records_one_file.map(
-        loader=loader,
-        file_path=file_list,
-        id_field=id_field,
-        subgraph_col=subgraph_col,
-        chunk_size=chunk_size
-    )
-    actual_results = [r.result() for r in futures]
-    
     return_dict = {}
-    for i in range(len(file_list)):
-        return_dict[file_list[i]] = actual_results[i]
-
+    for file in file_list:
+        print(f"Files to be processed: {file}")
+        record_upsert_summary=loader.upsert_file_records(
+            file_path=file,
+            id_field=id_field,
+            subgraph_col=subgraph_col,
+            chunk_size=chunk_size,
+        )
+        return_dict[file] = record_upsert_summary
     return return_dict
 
 
-@task(name="Upsert relationships of a file", log_prints=True, cache_policy=NO_CACHE)
-def upsert_rels_one_file(
-    loader: Loader,
-    file_path: str,
-    model_parser: ModelParser,
-    id_field: str,
-    chunk_size: int = 3000,
-    delimiter: str = ";",
-):
-    """Prefect task to upsert data relationships from a submission file
-
-    Args:
-        loader (Loader): Loader instance
-        file_path (str): submission file path
-        model_parser (ModelParser): ModelParser instance
-        id_field (str): id field to use for matching purpose
-        subgraph_col (str | None): The column indicating subgraph information. Defaults to None.
-        chunk_size (int, optional): Chunk size of each processing. Defaults to 3000.
-        delimiter (str, optional): Delimiter for multi-valued linkage fields. Defaults to ";"
-    """
-    file_upsert_summary = loader.upsert_file_relationships(
-        file_path=file_path,
-        model_parser=model_parser,
-        id_field=id_field,
-        chunk_size=chunk_size,
-        delimiter=delimiter,
-    )
-    return file_upsert_summary
+#@task(name="Upsert relationships of a file", log_prints=True, cache_policy=NO_CACHE)
+#def upsert_rels_one_file(
+#    loader: Loader,
+#    file_path: str,
+#    model_parser: ModelParser,
+#    id_field: str,
+#    chunk_size: int = 3000,
+#    delimiter: str = ";",
+#):
+#    """Prefect task to upsert data relationships from a submission file
+#
+#    Args:
+#        loader (Loader): Loader instance
+#        file_path (str): submission file path
+#        model_parser (ModelParser): ModelParser instance
+#        id_field (str): id field to use for matching purpose
+#        subgraph_col (str | None): The column indicating subgraph information. Defaults to None.
+#        chunk_size (int, optional): Chunk size of each processing. Defaults to 3000.
+#        delimiter (str, optional): Delimiter for multi-valued linkage fields. Defaults to ";"
+#    """
+#    file_upsert_summary = loader.upsert_file_relationships(
+#        file_path=file_path,
+#        model_parser=model_parser,
+#        id_field=id_field,
+#        chunk_size=chunk_size,
+#        delimiter=delimiter,
+#    )
+#    return file_upsert_summary
 
 
 @flow(
@@ -243,21 +237,17 @@ def upsert_rels_file_list(
         chunk_size (int, optional): Chunk size of each processing. Defaults to 3000.
         delimiter (str, optional): Delimiter for multi-valued linkage fields. Defaults to ";"
     """
-    # Submit all tasks first
-    futures = upsert_rels_one_file.map(
-        loader=loader,
-        file_path=file_list,
-        model_parser=model_parser,
-        id_field=id_field,
-        chunk_size=chunk_size,
-        delimiter=delimiter
-    )
-    actual_results = [r.result() for r in futures]
-
-    # Wait for all futures to complete and collect results
     return_dict = {}
-    for i in range(len(file_list)):
-        return_dict[file_list[i]] = actual_results[i]
+    for file in file_list:
+        print(f"Files to be processed: {file}")
+        rel_upsert_summary = loader.upsert_file_relationships(
+            file_path=file,
+            model_parser=model_parser,
+            id_field=id_field,
+            chunk_size=chunk_size,
+            delimiter=delimiter,
+        )
+        return_dict[file] = rel_upsert_summary
     return return_dict
 
 
@@ -328,15 +318,15 @@ def upsert_files(
     #     node_upsert_summary[file] = myloader.upsert_file_records(
     #         file_path=file, id_field=id_field, subgraph_col=subgraph_col, chunk_size=3000
     #     )
-    # node_upsert_summary = upsert_records_file_list(
-    #     loader=myloader,
-    #     file_list=file_list,
-    #     id_field=id_field,
-    #     subgraph_col=subgraph_col,
-    #     chunk_size=3000,
-    # )
-    # print("Print out node upsert summary:")
-    # print(json.dumps(node_upsert_summary, indent=4))
+    node_upsert_summary = upsert_records_file_list(
+        loader=myloader,
+        file_list=file_list,
+        id_field=id_field,
+        subgraph_col=subgraph_col,
+        chunk_size=3000,
+    )
+    print("Print out node upsert summary:")
+    print(json.dumps(node_upsert_summary, indent=4))
 
     # second to load all the relationships
     # rel_upsert_summary = {}
@@ -358,21 +348,21 @@ def upsert_files(
     # close myloader instance when the upload is done
     myloader.close()
 
-    ## combine two summaries into one, and write into a tsv
-    ## needs to combine two dict for every file
-    #combined_summary = combine_summaries(node_upsert_summary, rel_upsert_summary)
-    ## combined_summary = {k: node_upsert_summary[k] + rel_upsert_summary[k] for k in node_upsert_summary}
-    #summary_output_name = f"MEVAL_upsert_summary_{datetime.now().strftime('%Y%m%d_%H%M%S')}.tsv"
-    #summary_df = pd.DataFrame.from_dict(combined_summary, orient="index")
-    #summary_df.index.name = "file_name"
-    #summary_df = summary_df.reset_index()
-    #summary_df["file_name"] = summary_df["file_name"].apply(lambda x: os.path.basename(x))
-    #summary_df.to_csv(summary_output_name, sep="\t", index=False)
-    ## upload the summaru tsv to s3
-    #output_bucket, output_key_prefix = parse_file_url(output_bucket_loc)
-    #file_ul(
-    #    bucket=output_bucket,
-    #    output_folder=output_key_prefix,
-    #    sub_folder="MEVAL_upsert_summaries",
-    #    newfile=summary_output_name,
-    #)
+    # combine two summaries into one, and write into a tsv
+    # needs to combine two dict for every file
+    combined_summary = combine_summaries(node_upsert_summary, rel_upsert_summary)
+    # combined_summary = {k: node_upsert_summary[k] + rel_upsert_summary[k] for k in node_upsert_summary}
+    summary_output_name = f"MEVAL_upsert_summary_{datetime.now().strftime('%Y%m%d_%H%M%S')}.tsv"
+    summary_df = pd.DataFrame.from_dict(combined_summary, orient="index")
+    summary_df.index.name = "file_name"
+    summary_df = summary_df.reset_index()
+    summary_df["file_name"] = summary_df["file_name"].apply(lambda x: os.path.basename(x))
+    summary_df.to_csv(summary_output_name, sep="\t", index=False)
+    # upload the summaru tsv to s3
+    output_bucket, output_key_prefix = parse_file_url(output_bucket_loc)
+    file_ul(
+        bucket=output_bucket,
+        output_folder=output_key_prefix,
+        sub_folder="MEVAL_upsert_summaries",
+        newfile=summary_output_name,
+    )
